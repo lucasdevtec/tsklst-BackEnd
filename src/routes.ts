35117fd397e98aa9301bf935181bcabb5738d782
Lucas Oliveira <lucasprogjr@gmail.com>
2024-07-01 22:13:59 -0300

    Feat: Criando rota get todas as tasks de um usuário.

--- src/routes.ts
+++ src/routes.ts
@@ -8,22 +8,22 @@ import { UpdateUserController } from "./controllers/users/UpdateUserController";
 import { CreateTaskController } from "./controllers/tasks/CreateTaskController";
 import { DeleteTaskController } from "./controllers/tasks/DeleteTaskController";
 import { UpdateTaskController } from "./controllers/tasks/UpdateTaskController";
+import { GetAllTasksController } from "./controllers/tasks/GetAllTasksController";
 
 const router = Router();
-
 router.get("/", (req: Request, res: Response) => {
   return res.json({ hello: "Bem Vindo ao Ao TaskManeger" });
 });
-
+// Users
 router.post("/users", new CreateUserController().handle);
 router.post("/users/session", new AuthUserController().handle);
 router.get("/me", isAuth, new DetailUserController().handle);
 router.delete("/me", isAuth, new DeleteUserController().handle);
 router.put("/me", isAuth, new UpdateUserController().handle);
-
+//Tasks
 router.post("/tasks", isAuth, new CreateTaskController().handle);
 router.delete("/tasks", isAuth, new DeleteTaskController().handle);
 router.put("/tasks", isAuth, new UpdateTaskController().handle);
-// router.get("/tasks", isAuth, new GetAllTasksController().handle);
+router.get("/tasks", isAuth, new GetAllTasksController().handle);
 
 export { router };
